f978f4fc1ec9c8233fb3c0b4308c468859f38036
Fabrizio De Bortoli <fabrizio.debortoli@microarea.it>
2019-07-25 23:15:46 +0200

    aws

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,19 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
+  const app = await NestFactory.create(AppModule);
+
   const serverConfig = config.get('server');
 
-  const logger = new Logger('bootstrap');
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: [serverConfig.origin] });
+    logger.log(`Accepting requests from orogin ${serverConfig.origin}`);
+  }
 
   const port = process.env.PORT || serverConfig.port;
-  const app = await NestFactory.create(AppModule);
   await app.listen(port);
 
   logger.log(`Application listening on port ${port}`);
